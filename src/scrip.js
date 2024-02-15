2b5e32d0bee07ebecb4293da30d24c560991255c
Guilherme-Santt <guisant@icloud.com>
2024-02-15 11:49:36 -0300

    Inserindo switch

--- src/scrip.js
+++ src/scrip.js
@@ -1,6 +1,7 @@
 function buttonVerify(){
     let nome = document.getElementById("nome").value;
     let senha = document.getElementById("senha").value;
+    let cor = document.getElementById("cor").value;
 
     if(nome == "" || nome == null){
         let p = document.getElementById("alert");
@@ -10,9 +11,24 @@ function buttonVerify(){
         let p = document.getElementById("alert");
         p.innerHTML = "O campo senha não pode ser vazio";
         p.style.color = "red";
+    }else if(cor == "" || cor == null){
+        let p = document.getElementById("alert");
+        p.innerHTML = "O campo cor não pode ser vazio";
+        p.style.color = "red";
     }else{
         let p = document.getElementById("alert");
         p.innerHTML = "Enviado com sucesso";
         p.style.color = "green";
     }
+    
+    switch(cor){
+        case  "verde":
+            document.body.backgroundcolor = "green";
+        case  "azul":
+            document.body.backgroundcolor = "blue";
+        default:
+            p = document.getElementById("alertCor");
+            p.innerHTML = "Não foi inserido nenhuma cor";
+    }   
+
 }
